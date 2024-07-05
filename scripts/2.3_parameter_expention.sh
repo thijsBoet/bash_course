8edb815f60e4dcee4153ed1197fb930ff6d398d9
Matthijs Boet <m.boet@chello.nl>
2024-07-05 17:52:19 +0200

    finished command substritution starting exercise

--- scripts/2.3_parameter_expention.sh
+++ scripts/2.3_parameter_expention.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
-name="Matthijs Boet"
+first_name="MATTHIJS"
+numbers=123456789
 
-echo ${name,}
+# Uncapitalizes first letter
+echo ${first_name,}
+
+# Uncapitalizes entire string
+echo ${first_name,,}
+
+# Capitalizes first letter
+echo ${USER^}
+
+# Capitalizes entire string
+echo ${USER^^}
+
+# Returns number of characters
+echo ${#USER}
+
+# Slice a string or number
+# ${parameter:offset:length}
+echo ${numbers:0:5}
+# Prints to the end when length value is missing
+echo ${numbers:6}
+# Acceps negative values with space
+echo ${numbers: -3:2}
 
 exit 0
---
+++ scripts/2.4_command_substitution.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+time=$(date +%H:%M:%S)
+
+echo "Hello $USER, the time right now is $time."
+
+exit 0
